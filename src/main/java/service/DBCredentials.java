199277217ecb258c08f84765bf7e3d2b554a45d7
Roksolana <roksolana.d22@gmail.com>
2016-12-08 13:54:06 +0200

    Delete DBCredentials.java

--- src/main/java/service/DBCredentials.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public class DBCredentials {
-    public final static String ip="127.0.0.1";
-    public final static String port="3306";
-    public final static String username = "notBadIdeaUser";
-    public final static String password = "NotBad";
-    public final static String database = "ideaservice";
-}
